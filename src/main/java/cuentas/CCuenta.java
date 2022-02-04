a2781949bb92386f300009fd0c7a772278d3edb9
Victor <Victor@DESKTOP-8PT4U0I.home>
2022-02-04 20:27:27 +0100

    Versión final  - Creado el git con varios commit hechos (y push para verse en github)  - Cambiado el nombre de la variable de main a "cuenta1"  - Atributos de CCuenta encapsulados  + Consultada la lista de versiones del git por comando y añadido al PDF de la entrega  + Método operativa_cuenta que engloba las operaciones con cuenta1 introducido  + Método operativa_cuenta ahora cuenta con un parámetro tipo float llamado cantidad que se usa en las operaciones  + Creado Javadoc y comentado en el PDF de la entrega  + Re-estructurado el formato de los cambios al hacer commit  + Reservada la primera linea para lo que se ve como "titulo" en GitHub ("-" = Estaba en la anterior versión) ("+" = Añadido en esta versión)

--- src/main/java/cuentas/CCuenta.java
+++ src/main/java/cuentas/CCuenta.java
@@ -1,16 +1,31 @@
 package cuentas;
-
+/**
+ * Es la clase en la que se definen los objetos tipo CCuenta
+ * @author Victor
+ */
 public class CCuenta {
-
+    /**
+     * Atributos del objeto CCuenta (ahora encapsulados)
+     */
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
+    /**
+     * Método constructor vacío
+     */
     public CCuenta()
     {
     }
 
+    /**
+     * Método constructor con datos a guardar
+     * @param nom nombre de titular
+     * @param cue identificador de cuenta
+     * @param sal saldo inicial de la cuenta
+     * @param tipo el tipo de cuenta
+    */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -22,14 +37,26 @@ public class CCuenta {
     {
         return getSaldo();
     }
+    
 
+    /**
+     * Método para ingresar dinero
+     * @param cantidad a aumentar el saldo
+     * @throws Exception si cantidad es negativa
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+    
+    /**
+     * Método para retirar dinero de la cuenta
+     * @param cantidad a restar de saldo
+     * @throws Exception si cantidad es negativa
+     */
+    
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
--- src/main/java/cuentas/Main.java
+++ src/main/java/cuentas/Main.java
@@ -9,15 +9,24 @@ public class Main {
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
+        
+        
+        
+        operativa_cuenta(cuenta1, 1000);
+    }
 
+    //Englobo las sentencias que operan con cuenta1 | Refractor -> introduce -> method
+    //Además añado el parámetro cantidad (aunque no tenga sentido sumar y restar la misma cantidad, se entiende que es un ejercicio tipo)
+
+    public static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
         try {
-            cuenta1.retirar(2300);
+            cuenta1.retirar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            cuenta1.ingresar(695);
+            cuenta1.ingresar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
